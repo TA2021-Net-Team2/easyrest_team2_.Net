e72e65fe849edaaab745fce57036b0dddb5f5e30
zaetsa12 <57453996+zaetsa12@users.noreply.github.com>
2022-01-11 11:33:18 +0200

    Add first test

---
+++ Team2.Net/PageObjects/WaiterPanel.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Team2.Net.Utilities;
+
+namespace Team2.Net.PageObjects
+{
+    public partial class WaiterPanel
+    {
+        private IWebDriver _webDriver;
+
+        public WaiterPanel(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        
+        public WaiterPanel CloseOrder()
+        {
+            SeleniumWaiters.WaitElement(_webDriver, _linkInProgress);
+            //ExplicitWaiters.WaitElementDisplayed(_webDriver, _linkInProgress);
+            _webDriver.FindElement(_linkInProgress).Click();
+
+            return new WaiterPanel(_webDriver);
+        }
+    }
+
+}
---
+++ Team2.Net/PageObjects/WaiterPanelLocators.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Team2.Net.PageObjects
+{
+    public partial class WaiterPanel
+    {
+        private readonly By _linkInProgress = By.CssSelector("a[href='/waiter/orders/In progress']");
+        //private readonly By _progressButton = By.XPath("//span[contains(text(),'In progress')]//following::button[1]");
+        
+    }
+}
